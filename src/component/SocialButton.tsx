b99f3419819fbbdd6c329d297a6982462637a0dd
Norookezi <norookezi@pandacrp.com>
2025-06-05 23:05:00 +0200

    style: :lipstick: Align items in SocialButton component for better layout

--- src/component/SocialButton.tsx
+++ src/component/SocialButton.tsx
@@ -13,7 +13,7 @@ export function SocialButton({ name, icon, className }: SocialButtonProps) {
         <Link
             to={{ pathname: `/${name.toLowerCase()}` }}
             replace={true}
-            className={`${className} bg-white/10 flex flex-col md:flex-row text-center px-4 py-2 m-2 md:ml-0 rounded-lg font-semibold shadow transition-colors duration-200`}
+            className={`${className} bg-white/10 flex flex-col md:flex-row items-center text-center px-4 py-2 m-2 md:ml-0 rounded-lg font-semibold shadow transition-colors duration-200`}
         >
             <FontAwesomeIcon icon={icon} className='text-lg mr-2' />
             <span className='text-white'>{name}</span>
