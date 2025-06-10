d88f347cf901aae4b406e5ee6482a32c2ab5ef18
Norookezi <norookezi@pandacrp.com>
2025-06-10 06:06:04 +0200

    style: 💄 Update NavItem styles for improved hover effect

--- src/component/NavItem.tsx
+++ src/component/NavItem.tsx
@@ -9,14 +9,14 @@ export function NavItem({ name, activeSection }: NavItemProps) {
         <li className={`flex flex-1 w-auto flex-col items-center relative group ${activeSection == 'hero' ? 'invert' : ''}`}>
             <a
                 href={`#${name.toLowerCase()}`}
-                className={`transition-colors px-2 py-1 rounded ${activeSection == name.toLowerCase() ? 'font-bold text-black' : 'text-neutral-500 group-hover:text-neutral-700'}`}
+                className={`transition-colors px-2 py-1 rounded ${activeSection == name.toLowerCase() ? 'font-bold text-black' : 'text-neutral-500 group-hover:text-neutral-800'}`}
             >
                 {name}
             </a>
             {activeSection == name.toLowerCase() &&
                 <span className="absolute bottom-0 h-[0.2rem] w-full block bg-black duration-150"></span>
             }
-            <span className="absolute bottom-0 h-[0.1rem] w-0 group-hover:w-full  block bg-neutral-500 duration-150"></span>
+            <span className="absolute bottom-0 h-[0.1rem] w-0 group-hover:w-full  block bg-neutral-800 duration-150"></span>
         </li>
     );
 }
