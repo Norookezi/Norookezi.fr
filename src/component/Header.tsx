3e194f47d41dcf86fcbe2fe5b05926c69c6f30d2
Norookezi <norookezi@pandacrp.com>
2025-06-10 07:55:02 +0200

    style: 💄 Refactor Header moved in his own component

---
+++ src/component/Header.tsx
@@ -0,0 +1,39 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { NavItem } from './NavItem';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { faBars, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+
+export function Header({ selectedSection = 'hero', navItems }: { selectedSection?: string, navItems?: string[] }) {
+    const [isDarkMode, setDarkMode] = useState<boolean>(localStorage.getItem('darkMode') === 'true');
+    const [isMenuOpen, setMenuOpen] = useState<boolean>(false);
+
+    useEffect(() => {
+        localStorage.setItem('darkMode', isDarkMode ? 'true' : 'false');
+
+        document.documentElement.classList.toggle('dark', isDarkMode);
+    }, [isDarkMode]);
+
+    return (
+        <>
+            <header className={'fixed duration-300 w-screen p-2 items-center top-0 left-0 z-50'}>
+                <div className='bg-gray-400/50 backdrop-blur-xs rounded-2xl px-4 py-2 items-center gap-2 flex justify-between relative'>
+                    <Link to={{ pathname: '/' }} onClick={() => { window.scrollTo({ top: 0 }); }} >
+                        <img title='Website logo' src="/assets/panda-128.png" className='w-12 h-12 invert dark:invert-0' alt="Norookezi's logo" />
+                    </Link>
+                    <ul className={`flex w-[30vw] min-w-[32rem] items-center max-md:flex-col max-md:bg-gray-200/50 max-md:dark:bg-gray-800/30 h-auto ${!isMenuOpen ? 'max-md:max-h-0 max-md:pb-0' : 'max-md:max-h-[100vh] max-md:pb-5'} overflow-clip  max-md:rounded-b-2xl max-md:duration-1000 max-md:backdrop-blur-md max-md:left-1/2 max-md:-translate-x-1/2 max-md:w-[90vw] max-md:*:p-2  gap-2 text-white font-semibold max-md:absolute top-full`}>
+                        {
+                            navItems && navItems.map((item, index) => (
+                                <NavItem key={index} name={item} activeSection={selectedSection} />
+                            ))
+                        }
+                    </ul>
+                    <div>
+                        <FontAwesomeIcon onClick={() => { setDarkMode(!isDarkMode); }} icon={isDarkMode ? faMoon : faSun} className='text-4xl text-white dark:text-black' />
+                        <FontAwesomeIcon onClick={() => { setMenuOpen(!isMenuOpen); }} icon={faBars} className='max-md:inline-block! hidden! text-4xl text-white dark:text-black ml-4' />
+                    </div>
+                </div>
+            </header>
+        </>
+    );
+} 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUp, faBars, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 import { Hero } from '../sections/Hero';
 import { useEffect, useState } from 'react';
 import { About } from '../sections/About';
@@ -8,20 +7,21 @@ import { Tech } from '../sections/Tech';
 import { Experiences } from '../sections/Experiences';
 import { Studies } from '../sections/Studies';
 import { Portfolio } from '../sections/Portfolio';
-import { NavItem } from '../component/NavItem';
+import { Header } from '../component/Header';
+
+const navItems: string[] = [
+    'About',
+    'Tech',
+    'Experiences',
+    'Studies',
+    'Portfolio'
+];
 
 export function Home() {
     const [scrollPosition, updateScroll] = useState<[number, number]>([0, 0]);
-    const [isDarkMode, setDarkMode] = useState<boolean>(false);
-    const [isMenuOpen, setMenuOpen] = useState<boolean>(false);
     const [selectedSection, setSelectedSection] = useState<string>('hero');
 
     addEventListener('scroll', () => { updateScroll([window.scrollX, window.scrollY]); });
-
-    useEffect(() => {
-        document.documentElement.classList.toggle('dark', isDarkMode);
-        localStorage.setItem('darkMode', isDarkMode ? 'true' : 'false');
-    }, [isDarkMode]);
 
     useEffect(() => {
         const sections = Array.from(document.querySelectorAll('section')) as HTMLElement[];
@@ -61,24 +61,7 @@ export function Home() {
     return (
         <>
             <FontAwesomeIcon onClick={() => { scrollTo({ top: 0, behavior: 'smooth' }); }} icon={faArrowUp} className={`text-4xl fixed bottom-0 right-0 m-5 z-[9999] text-white rounded-full bg-white/50 aspect-square w-8 h-8! p-3 ${scrollPosition[1] > 100 ? 'block!' : 'hidden!'}`} aria-label="Scroll to the top" />
-            <header className={'fixed duration-300 w-screen p-2 items-center top-0 left-0 z-50'}>
-                <div className='bg-gray-400/50 backdrop-blur-xs rounded-2xl px-4 py-2 items-center gap-2 flex justify-between relative'>
-                    <Link to={{ pathname: '/' }} onClick={() => { window.scrollTo({ top: 0 }); }} >
-                        <img title='Website logo' src="/assets/panda-128.png" className='w-12 h-12 invert dark:invert-0' alt="Norookezi's logo" />
-                    </Link>
-                    <ul className={`flex w-[30vw] min-w-[32rem] items-center max-md:flex-col max-md:bg-gray-200/50 max-md:dark:bg-gray-800/30 h-auto ${!isMenuOpen ? 'max-md:max-h-0 max-md:pb-0' : 'max-md:max-h-[100vh] max-md:pb-5'} overflow-clip  max-md:rounded-b-2xl max-md:duration-1000 max-md:backdrop-blur-md max-md:left-1/2 max-md:-translate-x-1/2 max-md:w-[90vw] max-md:*:p-2  gap-2 text-white font-semibold max-md:absolute top-full`}>
-                        <NavItem name="About" activeSection={selectedSection} />
-                        <NavItem name="Tech" activeSection={selectedSection} />
-                        <NavItem name="Experiences" activeSection={selectedSection} />
-                        <NavItem name="Studies" activeSection={selectedSection} />
-                        <NavItem name="Portfolio" activeSection={selectedSection} />
-                    </ul>
-                    <div>
-                        <FontAwesomeIcon onClick={() => { setDarkMode(!isDarkMode); }} icon={isDarkMode ? faMoon : faSun} className='text-4xl text-white dark:text-black' />
-                        <FontAwesomeIcon onClick={() => { setMenuOpen(!isMenuOpen); }} icon={faBars} className='max-md:inline-block! hidden! text-4xl text-white dark:text-black ml-4' />
-                    </div>
-                </div>
-            </header>
+            <Header selectedSection={selectedSection} navItems={navItems} />
             <Hero isSelected={selectedSection == 'hero'} />
             <About isSelected={selectedSection == 'about'} />
             <Tech isSelected={selectedSection == 'technology'} />
