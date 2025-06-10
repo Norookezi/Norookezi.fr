7dcf3a017799815179b4ffd82f0049de9fcdbcf1
Norookezi <norookezi@pandacrp.com>
2025-06-10 06:45:25 +0200

    style: 💄 Update link behavior in header for smoother scrolling experience

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -63,11 +63,10 @@ export function Home() {
             <FontAwesomeIcon onClick={() => { scrollTo({ top: 0, behavior: 'smooth' }); }} icon={faArrowUp} className={`text-4xl fixed bottom-0 right-0 m-5 z-[9999] text-white rounded-full bg-white/50 aspect-square w-8 h-8! p-3 ${scrollPosition[1] > 100 ? 'block!' : 'hidden!'}`} aria-label="Scroll to the top" />
             <header className={'fixed duration-300 w-screen p-2 items-center top-0 left-0 z-50'}>
                 <div className='bg-gray-400/50 backdrop-blur-xs rounded-2xl px-4 py-2 items-center gap-2 flex justify-between relative'>
-                    <Link to="/" >
+                    <Link to={{ pathname: '/' }} onClick={() => { window.scrollTo({ top: 0 }); }} >
                         <img title='Website logo' src="/assets/panda-128.png" className='w-12 h-12 invert dark:invert-0' alt="Norookezi's logo" />
                     </Link>
                     <ul className={`flex w-[30vw] min-w-[32rem] items-center max-md:flex-col max-md:bg-gray-200/50 max-md:dark:bg-gray-800/30 h-auto ${!isMenuOpen ? 'max-md:max-h-0 max-md:pb-0' : 'max-md:max-h-[100vh] max-md:pb-5'} overflow-clip  max-md:rounded-b-2xl max-md:duration-1000 max-md:backdrop-blur-md max-md:left-1/2 max-md:-translate-x-1/2 max-md:w-[90vw] max-md:*:p-2  gap-2 text-white font-semibold max-md:absolute top-full`}>
-                        <NavItem name="Hero" activeSection={selectedSection} />
                         <NavItem name="About" activeSection={selectedSection} />
                         <NavItem name="Tech" activeSection={selectedSection} />
                         <NavItem name="Experiences" activeSection={selectedSection} />
