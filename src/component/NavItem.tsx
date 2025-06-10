d296b0674d17c3af4ed416535e4bf185fa4f577c
Norookezi <norookezi@pandacrp.com>
2025-06-10 06:36:28 +0200

    style: 💄 Refactor NavItem styles for improved UI consistency and add navigation menu in Home component

--- src/component/NavItem.tsx
+++ src/component/NavItem.tsx
@@ -4,19 +4,20 @@ interface NavItemProps {
 }
 
 export function NavItem({ name, activeSection }: NavItemProps) {
+    const isHero = activeSection === 'hero';
 
     return (
-        <li className={`flex flex-1 w-auto flex-col items-center relative group ${activeSection == 'hero' ? 'invert' : ''}`}>
+        <li className="flex flex-1 w-auto flex-col items-center relative group">
             <a
                 href={`#${name.toLowerCase()}`}
-                className={`transition-colors px-2 py-1 rounded ${activeSection == name.toLowerCase() ? 'font-bold text-black' : 'text-neutral-500 group-hover:text-neutral-800'}`}
+                className={`transition-colors px-2 py-1 rounded ${activeSection == name.toLowerCase() ? `font-bold ${!isHero ? 'text-black' : 'text-white'}` : `${!isHero ? 'text-neutral-500' : 'text-neutral-200'} group-hover:text-neutral-800`}`}
             >
                 {name}
             </a>
             {activeSection == name.toLowerCase() &&
-                <span className="absolute bottom-0 h-[0.2rem] w-full block bg-black duration-150"></span>
+                <span className={`absolute bottom-0 h-[0.2rem] w-full block ${!isHero ? 'bg-black' : 'bg-white'} duration-150`}></span>
             }
-            <span className="absolute bottom-0 h-[0.1rem] w-0 group-hover:w-full  block bg-neutral-800 duration-150"></span>
+            <span className={`absolute bottom-0 h-[0.1rem] w-0 group-hover:w-full  block ${!isHero ? 'bg-neutral-800' : 'bg-neutral-500'} duration-150`}></span>
         </li>
     );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUp, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { faArrowUp, faBars, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import { Hero } from '../sections/Hero';
 import { useEffect, useState } from 'react';
@@ -8,10 +8,12 @@ import { Tech } from '../sections/Tech';
 import { Experiences } from '../sections/Experiences';
 import { Studies } from '../sections/Studies';
 import { Portfolio } from '../sections/Portfolio';
+import { NavItem } from '../component/NavItem';
 
 export function Home() {
     const [scrollPosition, updateScroll] = useState<[number, number]>([0, 0]);
     const [isDarkMode, setDarkMode] = useState<boolean>(false);
+    const [isMenuOpen, setMenuOpen] = useState<boolean>(false);
     const [selectedSection, setSelectedSection] = useState<string>('hero');
 
     addEventListener('scroll', () => { updateScroll([window.scrollX, window.scrollY]); });
@@ -59,12 +61,23 @@ export function Home() {
     return (
         <>
             <FontAwesomeIcon onClick={() => { scrollTo({ top: 0, behavior: 'smooth' }); }} icon={faArrowUp} className={`text-4xl fixed bottom-0 right-0 m-5 z-[9999] text-white rounded-full bg-white/50 aspect-square w-8 h-8! p-3 ${scrollPosition[1] > 100 ? 'block!' : 'hidden!'}`} aria-label="Scroll to the top" />
-            <header className={'fixed duration-300 w-screen p-2 items-center top-0 left-0 z-50 dark:invert'}>
-                <div className='bg-gray-400/50 backdrop-blur-xs rounded-2xl px-4 py-2 items-center gap-2 flex justify-between'>
+            <header className={'fixed duration-300 w-screen p-2 items-center top-0 left-0 z-50'}>
+                <div className='bg-gray-400/50 backdrop-blur-xs rounded-2xl px-4 py-2 items-center gap-2 flex justify-between relative'>
                     <Link to="/" >
-                        <img title='Website logo' src="/assets/panda-128.png" className='w-12 h-12 invert' alt="Norookezi's logo" />
+                        <img title='Website logo' src="/assets/panda-128.png" className='w-12 h-12 invert dark:invert-0' alt="Norookezi's logo" />
                     </Link>
-                    <FontAwesomeIcon onClick={() => { setDarkMode(!isDarkMode); }} icon={isDarkMode ? faMoon : faSun} className='text-4xl text-white' />
+                    <ul className={`flex w-[30vw] min-w-[32rem] items-center max-md:flex-col max-md:bg-gray-200/50 max-md:dark:bg-gray-800/30 h-auto ${!isMenuOpen ? 'max-md:max-h-0 max-md:pb-0' : 'max-md:max-h-[100vh] max-md:pb-5'} overflow-clip  rounded-b-2xl duration-1000 max-md:backdrop-blur-md max-md:left-1/2 max-md:-translate-x-1/2 max-md:w-[90vw] max-md:*:p-2  gap-2 text-white font-semibold max-md:absolute top-full`}>
+                        <NavItem name="Hero" activeSection={selectedSection} />
+                        <NavItem name="About" activeSection={selectedSection} />
+                        <NavItem name="Tech" activeSection={selectedSection} />
+                        <NavItem name="Experiences" activeSection={selectedSection} />
+                        <NavItem name="Studies" activeSection={selectedSection} />
+                        <NavItem name="Portfolio" activeSection={selectedSection} />
+                    </ul>
+                    <div>
+                        <FontAwesomeIcon onClick={() => { setDarkMode(!isDarkMode); }} icon={isDarkMode ? faMoon : faSun} className='text-4xl text-white dark:text-black' />
+                        <FontAwesomeIcon onClick={() => { setMenuOpen(!isMenuOpen); }} icon={faBars} className='text-4xl text-white dark:text-black ml-4' />
+                    </div>
                 </div>
             </header>
             <Hero isSelected={selectedSection == 'hero'} />
